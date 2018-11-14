cab7d7ecae02073b2a9e6208597565a1fa26c9e5
Chan Lee and David Zhu <chan@>
2018-11-14 09:56:49 -0800

    finished

---
+++ src/components/MyStyledComponent.jsx
@@ -0,0 +1,14 @@
+function MyStyledComponent() {
+  return (
+    <div>
+      <p className="red-text">only this paragraph will get the style :)</p>
+      <style jsx>{`
+        .red-text {
+          color: red;
+        }
+    `}</style>
+    </div>
+  );
+}
+
+export default MyStyledComponent;
--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Ticket(props){
-  var myStyledComponentStyles = {
-    backgroundColor: '#ccc',
-    fontFamily: 'sans-serif',
-    paddingTop: '50px'
-  };
+  // var myStyledComponentStyles = {
+  //   backgroundColor: '#ccc',
+  //   fontFamily: 'sans-serif',
+  //   paddingTop: '50px'
+  // };
 
   return (
-    <div style={ myStyledComponentStyles }>
+    <div >
+      <style jsx>{`div {
+          background-color: red;
+        }
+      `}
+      </style>
       <h3>{props.location} - {props.names}</h3>
       <p><em>{props.issue}</em></p>
       <hr/>

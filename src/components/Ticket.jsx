0ab12d7ec4c754a1dba034bf4fe453f7433b081d
Chan Lee and David Zhu <david@>
2018-11-14 09:28:16 -0800

    add linting.

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,19 +1,19 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
 function Ticket(props){
   var myStyledComponentStyles = {
     backgroundColor: '#ccc',
     fontFamily: 'sans-serif',
     paddingTop: '50px'
-  }
+  };
 
   return (
-      <div style={ myStyledComponentStyles }>
-        <h3>{props.location} - {props.names}</h3>
-        <p><em>{props.issue}</em></p>
-        <hr/>
-      </div>
+    <div style={ myStyledComponentStyles }>
+      <h3>{props.location} - {props.names}</h3>
+      <p><em>{props.issue}</em></p>
+      <hr/>
+    </div>
   );
 }
 
